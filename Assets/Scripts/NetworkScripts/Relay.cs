f5531a229ddbbd3a8645dca759e0b65d6edd6488
Tyrel Ratima <tyrel.ratima@gmail.com>
2025-10-05 16:11:02 +1300

    added cinemanchine
    other scripts aswell

---
+++ Assets/Scripts/NetworkScripts/Relay.cs
@@ -0,0 +1,83 @@
+using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using UnityEngine;
+
+public class TestRelay : MonoBehaviour
+{
+    public static TestRelay Instance { get; private set; }
+
+    public int MaxPlayers = 3;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        
+    }
+
+
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log("Signed in! Player ID: " + AuthenticationService.Instance.PlayerId);
+        };
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    public async Task<string> CreateRelay()
+    {
+        try
+        {
+            // Treat MaxPlayers as TOTAL (host + clients)
+            int clientSlots = Mathf.Max(1, MaxPlayers - 1); // Relay requires at least 1
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(clientSlots);
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            Debug.Log("Relay allocation created. Join code: " + joinCode);
+
+            var connectionType = "dtls";
+            var relayServerData = allocation.ToRelayServerData(connectionType);
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+            NetworkManager.Singleton.StartHost();
+            return joinCode;
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.LogError("Failed to create Relay allocation: " + e);
+            return null;
+        }
+
+    }
+
+    public async void JoinRelay(string joinCode)
+    {
+        try
+        {
+            Debug.Log("Joining Relay with " + joinCode);
+            JoinAllocation joinAllocation =  await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+            var connectionType = "dtls";
+            var relayServerData = joinAllocation.ToRelayServerData(connectionType);
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            NetworkManager.Singleton.StartClient();
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.LogError("Failed to join Relay allocation: " + e);
+        }
+    }
+
+
+}
